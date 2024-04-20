fae295ce40f5c1df09330821d98b33270534bb53
paudelanil <paudelanil9@gmail.com>
2024-04-20 15:38:54 +0545

    delete earlier

--- BackEnd/mlmodel/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ImageModel
-
-class ImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ImageModel
-        fields = ["image"]
--- BackEnd/mlmodel/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import UseMlModel,UseMlModelTemp,DetectEmotion
-
-urlpatterns = [path("mlmodel", UseMlModel, name="mlmodel"),
-               path("detect",DetectEmotion,name= 'detect'),
-               ]
